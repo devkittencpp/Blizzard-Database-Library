c30ccbfd070d3e1bd5d82727ab3f61f85b1acf96
Alister <alister.greg@gmail.com>
2021-08-03 18:53:36 +0100

    Expanding Streamreader class
    Expanding Streamreader class to support functionality that is needed for reading db2 files.

--- src/SteamReader.cpp
+++ src/SteamReader.cpp
@@ -5,17 +5,47 @@ StreamReader::StreamReader(std::istream& stream) : _underlyingStream(stream)
 
 }
 
-void StreamReader::Jump(long position)
+std::string StreamReader::ReadString()
+{
+   std::string string;
+   std::getline(_underlyingStream, string, '\0');
+   return string;
+}
+
+std::unique_ptr<char[]> StreamReader::ReadBlock(std::size_t length)
+{
+    std::unique_ptr<char[]> blockArray(new char[length]);
+     _underlyingStream.read(blockArray.get(),length);
+
+    return blockArray;
+}
+  
+std::streampos StreamReader::Position()
+{
+  return _underlyingStream.tellg();
+}
+
+void StreamReader::JumpEnd()
+{
+    _underlyingStream.seekg(0,_underlyingStream.end);
+}
+
+void StreamReader::JumpStart()
+{
+    _underlyingStream.seekg(0,_underlyingStream.beg);
+}
+
+void StreamReader::Jump(std::streampos position)
 {
    _underlyingStream.seekg(position);
 }
 
 std::size_t StreamReader::Length()
 {
-    auto currentPosition = _underlyingStream.tellg();
-    _underlyingStream.seekg(0,_underlyingStream.end);
-    auto length = _underlyingStream.tellg();
-    _underlyingStream.seekg(currentPosition);
+    auto currentPosition = Position();
+    JumpEnd();
+    auto length = Position();
+    Jump(currentPosition);
 
     return length;
 }
--- src/StreamReader.h
+++ src/StreamReader.h
@@ -1,6 +1,17 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <string>
+
+
+struct MemoryBuffer : std::streambuf
+{
+public:
+    MemoryBuffer(char* firstElement, char* LastElement) 
+    {
+        this->setg(firstElement, firstElement, LastElement);
+    }
+};
 
 class StreamReader
 {
@@ -12,23 +23,26 @@ public:
     template <typename T> T Read()
     {
         auto type = T();
-        auto size = sizeof(T);
+        auto size = sizeof(T);    
         auto pointer = reinterpret_cast<char*>(&type);
         _underlyingStream.read(pointer, size);
-
         return type;
     }
 
-    template <typename T> T ReadArray(std::size_t length)
+    template <typename T> std::vector<T> ReadArray(std::size_t length)
     {
         auto vector = std::vector<T>(length);
         auto size = sizeof(T);
         auto pointer = reinterpret_cast<char*>(vector.data());
-        _underlyingStream.read(pointer, size);
-
+        _underlyingStream.read(pointer, size * length);
         return vector;
     }
-    
-    void Jump(long position);
+
+    std::unique_ptr<char[]> ReadBlock(std::size_t length);
+    std::string ReadString();
+    void Jump(std::streampos position);
+    void JumpEnd();
+    void JumpStart();
     std::size_t Length();
+    std::streampos Position();
 };
