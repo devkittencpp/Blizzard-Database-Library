7f59dd3c7098cb51c0993febc1d723647834036b
Alister <alister.greg@gmail.com>
2021-08-03 21:35:47 +0100

    added definitions for other file structure types
    added definitions for other file structure types

--- src/DatabaseDefinitionsLib/FileStructures.h
+++ src/DatabaseDefinitionsLib/FileStructures.h
@@ -1,5 +1,17 @@
 #pragma once
 #include <string>
+#include <map>
+#include <vector>
+
+class Build
+{
+    //Populate later
+};
+
+class BuildRange
+{
+    //Populate later
+};
 
 struct ColumnDefinition
 {
@@ -8,4 +20,31 @@ struct ColumnDefinition
     std::string foreignColumn;
     bool verified;
     std::string comment;
+};
+
+struct Definition
+{
+    int size;
+    int arrLength;
+    std::string name;
+    bool isID;
+    bool isRelation;
+    bool isNonInline;
+    bool isSigned;
+    std::string comment;
+};
+
+struct DBDefinition
+{
+    std::map<std::string, ColumnDefinition> columnDefinitions;
+    std::vector<VersionDefinitions> versionDefinitions;
+};
+
+struct VersionDefinitions
+{
+    std::vector<Build> builds;
+    std::vector<BuildRange> buildRanges;
+    std::vector<std::string> layoutHashes;
+    std::string comment;
+    std::vector<Definition> definitions;
 };
