76665e4c5c38cd0d95b73dc38f8e3bbe4a03a155
Alister <alister.greg@gmail.com>
2021-08-03 23:25:27 +0100

    Added Required structs to store build and build range
    Added Required structs to store build and build range

--- src/DatabaseDefinitionsLib/FileStructures.h
+++ src/DatabaseDefinitionsLib/FileStructures.h
@@ -2,15 +2,35 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <Shared.h>
 
 class Build
 {
-    //Populate later
+private:
+    short _expansion;
+    short _major;
+    short _minor;
+    unsigned int build;
+public:
+    Build(std::string buildString)
+    {
+        auto buildVersionElements = StringExtenstions::Split(buildString,'.');
+
+        _expansion = std::atoi(buildVersionElements[0].c_str());
+        _major = std::atoi(buildVersionElements[1].c_str());
+        _minor = std::atoi(buildVersionElements[2].c_str());
+        build = std::atoi(buildVersionElements[3].c_str());
+    }
+
 };
 
 class BuildRange
 {
-    //Populate later
+private:
+    Build _minBuild;
+    Build _maxBuild;
+public:
+    BuildRange(Build minBuild, Build maxBuild) : _minBuild(minBuild), _maxBuild(_maxBuild) {}
 };
 
 struct ColumnDefinition
@@ -29,15 +49,9 @@ struct Definition
     std::string name;
     bool isID;
     bool isRelation;
-    bool isNonInline;
+    bool IsInline;
     bool isSigned;
     std::string comment;
-};
-
-struct DBDefinition
-{
-    std::map<std::string, ColumnDefinition> columnDefinitions;
-    std::vector<VersionDefinitions> versionDefinitions;
 };
 
 struct VersionDefinitions
@@ -48,3 +62,10 @@ struct VersionDefinitions
     std::string comment;
     std::vector<Definition> definitions;
 };
+
+struct DBDefinition
+{
+    std::map<std::string, ColumnDefinition> columnDefinitions;
+    std::vector<VersionDefinitions> versionDefinitions;
+};
+
