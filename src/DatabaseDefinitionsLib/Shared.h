4fb29756dcc0238ff3c413b3bf4e1707569edee2
Alister <alister.greg@gmail.com>
2021-08-07 12:47:31 +0100

    added std::tab, so I can indent text streams
    added std::tab, so I can indent text streams

--- src/DatabaseDefinitionsLib/Shared.h
+++ src/DatabaseDefinitionsLib/Shared.h
@@ -3,6 +3,14 @@
 #include <string>
 #include <sstream>
 
+namespace std {
+    template <typename _CharT, typename _Traits>
+    inline basic_ostream<_CharT, _Traits>&
+        tab(basic_ostream<_CharT, _Traits>& __os) {
+        return __os.put(__os.widen('\t'));
+    }
+}
+
 class StringExtenstions
 {
 public:
