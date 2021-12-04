5c0458b0697f0faddac2afc242600c9e80b38f6d
Alister <alister.greg@gmail.com>
2021-12-04 11:15:40 +0000

    setting up test console to open WDBC file

--- test/main.cpp
+++ test/main.cpp
@@ -9,6 +9,7 @@
 int main(int argc, char* argv[])
 {
     std::string dbcFileDirectory = std::string();
+    std::string dbcWrathFileDirectory = std::string();
     std::string dbdFileDirectory = std::string();
 
     for(auto index =0; index < argc; index++)
@@ -32,6 +33,16 @@ int main(int argc, char* argv[])
             dbdFileDirectory = argument.substr(5, argument.size() - 5);
             std::cout << "Database Definitions Directory Found: " << dbdFileDirectory << std::endl;
         }
+
+        argument = std::string(argv[index]);
+        path = std::string("-DWBP");
+        match = strncmp(argument.c_str(), path.c_str(), path.size());
+
+        if (match == 0) //match found
+        {
+            dbcWrathFileDirectory = argument.substr(6, argument.size() - 6);
+            std::cout << "Database Directory Wrath Found: " << dbcWrathFileDirectory << std::endl;
+        }
     }
 
     if(dbcFileDirectory.empty())
@@ -47,42 +58,55 @@ int main(int argc, char* argv[])
     }
 
     const auto& build = BlizzardDatabaseLib::Structures::Build("9.1.0.39584");
+    const auto& build_wrath = BlizzardDatabaseLib::Structures::Build("3.3.5.12340");
     const auto& table = std::string("map");
     const auto& sparseTable = std::string("ItemSparse");
 
-
-    auto blizzardDatabase = BlizzardDatabaseLib::BlizzardDatabase(dbcFileDirectory, dbdFileDirectory);
-    auto mapTable = blizzardDatabase.LoadTable(table, build);
-
-    auto DG = mapTable.Record(2245); //Deepwind Gorge
-    auto AV = mapTable.Record(30); //Alterac Valley
-    auto AC = mapTable.Record(37); //Azshara Crater
+    auto blizzardDatabase = BlizzardDatabaseLib::BlizzardDatabase(dbcWrathFileDirectory, dbdFileDirectory);
+    auto mapTable = blizzardDatabase.LoadTable(table, build_wrath);
 
     auto iterator = mapTable.Records();
     while (iterator.HasRecords())
     {
         auto record = iterator.Next();
-
+    
         if(!record.Columns.empty())
             std::cout << record.RecordId << "=>" << record.Columns.at("MapName_lang").Value << std::endl;
     }
 
-    auto newRecordDefinition = mapTable.GetRecordDefinition();
-    newRecordDefinition = AV;
 
-    auto newColumn = BlizzardDatabaseLib::Structures::BlizzardDatabaseColumn();
-    newColumn.Value = std::string("Custom AV");
-    newRecordDefinition.Columns["MapName_lang"] = newColumn;
-   
-    mapTable.WriterRecord(newRecordDefinition);
+   //auto blizzardDatabase = BlizzardDatabaseLib::BlizzardDatabase(dbcFileDirectory, dbdFileDirectory);
+   //auto mapTable = blizzardDatabase.LoadTable(table, build);
 
-    AC = mapTable.Record(37); //Azshara Crater
-
-    blizzardDatabase.UnloadTable(table);
-
-    auto itemTable = blizzardDatabase.LoadTable(sparseTable, build);
-    auto item = itemTable.Record(187111); //Memory of Blind Faith
-    blizzardDatabase.UnloadTable(sparseTable);
+   //auto DG = mapTable.Record(2245); //Deepwind Gorge
+   //auto AV = mapTable.Record(30); //Alterac Valley
+   //auto AC = mapTable.Record(37); //Azshara Crater
+   //
+   //auto iterator = mapTable.Records();
+   //while (iterator.HasRecords())
+   //{
+   //    auto record = iterator.Next();
+   //
+   //    if(!record.Columns.empty())
+   //        std::cout << record.RecordId << "=>" << record.Columns.at("MapName_lang").Value << std::endl;
+   //}
+   //
+   //auto newRecordDefinition = mapTable.GetRecordDefinition();
+   //newRecordDefinition = AV;
+   //
+   //auto newColumn = BlizzardDatabaseLib::Structures::BlizzardDatabaseColumn();
+   //newColumn.Value = std::string("Custom AV");
+   //newRecordDefinition.Columns["MapName_lang"] = newColumn;
+   //
+   //mapTable.WriterRecord(newRecordDefinition);
+   //
+   //AC = mapTable.Record(37); //Azshara Crater
+   //
+   //blizzardDatabase.UnloadTable(table);
+   //
+   //auto itemTable = blizzardDatabase.LoadTable(sparseTable, build);
+   //auto item = itemTable.Record(187111); //Memory of Blind Faith
+   //blizzardDatabase.UnloadTable(sparseTable);
 
     //auto achievementTable = blizzardDatabase.LoadTable("achievement");
     //auto areapoiTable = blizzardDatabase.LoadTable("areapoi");
